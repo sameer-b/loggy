1f0bdb067232f9e30c0180e5d397d469c4f9c231
Sameer <bs.sameer1@gmail.com>
2016-11-17 14:58:45 -0500

    2 -> 4 spaces

--- gulpfile.js
+++ gulpfile.js
@@ -4,29 +4,29 @@ const del = require('del');
 const exec = require('child_process').exec;
 
 const paths = {
-  allSrcJs: 'src/**/*.js',
-  libDir: 'lib',
+    allSrcJs: 'src/**/*.js',
+    libDir: 'lib',
 };
 
 gulp.task('clean', () => {
-  return del(paths.libDir);
+    return del(paths.libDir);
 });
 
 gulp.task('build', ['clean'], () => {
-  return gulp.src(paths.allSrcJs)
+    return gulp.src(paths.allSrcJs)
     .pipe(babel())
     .pipe(gulp.dest(paths.libDir));
 });
 
 gulp.task('main', ['build'], (callback) => {
-  exec(`node ${paths.libDir}`, (error, stdout) => {
-    console.log(stdout);
-    return callback(error);
-  });
+    exec(`node ${paths.libDir}`, (error, stdout) => {
+        console.log(stdout);
+        return callback(error);
+    });
 });
 
 gulp.task('watch', () => {
-  gulp.watch(paths.allSrcJs, ['main']);
+    gulp.watch(paths.allSrcJs, ['main']);
 });
 
 gulp.task('default', ['watch', 'main']);
