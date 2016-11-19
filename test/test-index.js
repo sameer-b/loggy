3781f31840a7bd2fef9bfb9c72ab41ab57d8a629
Sameer <bs.sameer1@gmail.com>
2016-11-19 13:48:05 -0500

    Added unit tests

---
+++ test/test-index.js
@@ -0,0 +1,17 @@
+
+/* global describe */
+describe('index.js tests', function() {
+    /* global it */
+    it('should call override console.log', function() {
+    });
+    /* global it */
+    it('should call override console.info', function() {
+    });
+    /* global it */
+    it('should call override console.warn', function() {
+    });
+    /* global it */
+    it('should call override console.error', function() {
+    });
+
+});
